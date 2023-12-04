ffcbcd7f261acfeba360d9b7f03fd518c13ee9e9
Hellen <hellen.aparecidams@outlook.com>
2023-12-04 20:57:07 -0300

    resolução do desafio 03 da aula 04

--- mundo_01/Desafios/desafio03.py
+++ mundo_01/Desafios/desafio03.py
@@ -1,2 +1,5 @@
 #Crie um script Python que leia dois números e tente mostrar a soma entre eles
-
+num1=input('Digite um número ')
+num2=input('Digite outro número ')
+soma=num1+num2
+print('A soma de' ,num1, '+' ,num2, 'é igual' ,int(num1)+int(num2))
