86850577ba12353b68c028b886618a8d042d55e3
Hellen <hellen.aparecidams@outlook.com>
2023-12-04 20:52:35 -0300

    resolução do desafio 02 da aula 04

--- mundo_01/Desafios/desafio02.py
+++ mundo_01/Desafios/desafio02.py
@@ -1,2 +1,6 @@
 #Crie um script Python que leia o dia, o mês e o ano
 #de um nascimento de uma pessoa e mostre uma mensagem com a data formatada
+dia=input('Em que dia você nasceu? ')
+mes=input('Em que mês você nasceu? ')
+ano=input('Em que ano você nasceu? ')
+print('Você nasceu no dia',dia, 'de',mes, 'de' ,ano,'!,Correto?')
