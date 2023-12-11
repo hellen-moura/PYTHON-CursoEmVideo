c20f7ce25717084d144073b6e58dc9f4357a6d6b
Hellen <hellen.aparecidams@outlook.com>
2023-12-11 20:37:49 -0300

    começando exercício 005

---
+++ mundo_01/PythonExercicios/ex004.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia algo pelo teclado e mostre na tela o seu 
+# tipo primitivo e todas as informações possíveis sobre ela.
+
+a = input ('Digite algo:')
+print('O tipo primitivo desse valor é', type(a))
+print()
