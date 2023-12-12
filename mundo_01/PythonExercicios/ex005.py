1abc970198b6b7ee2b71e75eee1db4ff9fb0451e
Hellen <hellen.aparecidams@outlook.com>
2023-12-12 19:50:56 -0300

    funcionou o programa de leitura dos números inteiros mostrando sucessor e antecessor

--- mundo_01/PythonExercicios/ex005.py
+++ mundo_01/PythonExercicios/ex005.py
@@ -8,4 +8,4 @@ print('Analisando o valor {}, seu antecessor é {} e o seu sucessor é {}' .form
 n = int(input('Digite um número:'))
 a = n - 1
 s = n + 1
-print('Analisando o valor {}')
+print('Analisando o valor {}, seu antecessor é {} e o seu sucessor é {}' .format(n,a,s))
