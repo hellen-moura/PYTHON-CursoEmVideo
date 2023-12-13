51cda10fbc8939a9cd4c192254c39bf1e06e7497
Hellen <hellen.aparecidams@outlook.com>
2023-12-12 21:39:23 -0300

    testando segunda lógica

--- mundo_01/PythonExercicios/ex006.py
+++ mundo_01/PythonExercicios/ex006.py
@@ -5,3 +5,5 @@ print('O dobro de {} vale {}!' .format(n, (n*2)))
 print('O triplo de {} vale {}. \nA raiz quadrada de {} é igual a {:.2f}'.format(n, (n*3), n, pow(n, (1/2))))
 
 #or
+
+n = int(input('Digite um número:'))
