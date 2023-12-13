feaf2015f2623862c75b0c0c1b53f6aac706418a
Hellen <hellen.aparecidams@outlook.com>
2023-12-12 21:41:50 -0300

    corrigindo erros

--- mundo_01/PythonExercicios/ex006.py
+++ mundo_01/PythonExercicios/ex006.py
@@ -7,3 +7,8 @@ print('O triplo de {} vale {}. \nA raiz quadrada de {} é igual a {:.2f}'.format
 #or
 
 n = int(input('Digite um número:'))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print('O dobro de {} vale {}' .format(n, d))
+print('O triplo de {} vale {}. \nA raiz quadrada de {} é igual a {:.2f}'.format(n, t, n, r))
