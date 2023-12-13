3d80779c16ede48f91e4ac07328887e64ca5c695
Hellen <hellen.aparecidams@outlook.com>
2023-12-12 21:36:47 -0300

    testando

--- mundo_01/PythonExercicios/ex006.py
+++ mundo_01/PythonExercicios/ex006.py
@@ -1,4 +1,5 @@
 # Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
 
 n = int(input('Digite um número:'))
-print('O dobro de {}')
+print('O dobro de {} vale {}!' .format(n, (n*2)))
+print('O triplo de {}')
