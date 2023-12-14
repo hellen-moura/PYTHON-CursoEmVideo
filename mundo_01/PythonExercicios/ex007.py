0550f8e8004e41d4f97f4fb5b5692f25c85f6ad7
Hellen <hellen.aparecidams@outlook.com>
2023-12-13 21:16:31 -0300

    finalizado com sucesso

--- mundo_01/PythonExercicios/ex007.py
+++ mundo_01/PythonExercicios/ex007.py
@@ -4,3 +4,10 @@ n1 = float(input('Digite a primeira nota do aluno: '))
 n1 = float(input('Digite a segunda nota do aluno: '))
 média= (n1 + n2) / 2
 print('A média entre {:.1f} é igual a {:.1f}' .format (n1, n2, média))
+
+
+#or
+
+n1 = float(input('Nota 1: '))
+n2 = float(input('Nota 2: '))
+print(f'Resultado: \n - Nota final: {n1+n2} \n - Média: {(n1+n2)/2:.1f}')
