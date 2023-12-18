54a6daa078651efcbbda40fd428c3d4e22bd4902
Hellen <hellen.aparecidams@outlook.com>
2023-12-18 20:54:01 -0300

    funcionou

--- mundo_01/PythonExercicios/ex008.py
+++ mundo_01/PythonExercicios/ex008.py
@@ -1,6 +1,7 @@
 # Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e
 # milímetros. --> (km   hm   dam   m     dm     cm   mm)
 
+
 medida = float(input('Digite uma distância em metros:'))
 km = medida / 1000
 hm = medida / 100
@@ -9,6 +10,7 @@ m = medida
 dm = medida * 10
 cm = medida * 100
 mm = medida * 1000
+
 
 print('A medida de {}m corresponde a:'.format(medida))
 print('{:.1f} km'.format(km))
