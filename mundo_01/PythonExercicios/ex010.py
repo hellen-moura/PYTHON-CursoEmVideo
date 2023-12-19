9d7312afdc692c4a6a355c16fec9d759d48d4d6c
Hellen <hellen.aparecidams@outlook.com>
2023-12-18 21:20:58 -0300

    testando e arrumadno erros

--- mundo_01/PythonExercicios/ex010.py
+++ mundo_01/PythonExercicios/ex010.py
@@ -1,6 +1,5 @@
 # Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
 
-# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
 
 real = float(input('Quanto dinheiro você tem na carteira? R$'))
 dolar = real / 4.89
